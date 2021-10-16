ab9ad2d71740f19e9e294081c4b4226041a07f9f
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2021-10-16 14:13:47 +0000

    [add2]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'be54f03064ae3e595fe1f5ce837777036739bf4f14538bb66c6f15642be45f933c4e518ba79858d22a9cc4b8f7a98ccb13661bc0400c6f324a8fd3bbff063182'
